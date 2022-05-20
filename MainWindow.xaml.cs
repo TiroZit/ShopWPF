67c58129d2cbc7032022c157c5909d146cc3e4e7
TiroZit <nikus.master@gmail.com>
2022-05-20 18:47:10 +0500

    load data

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Shop.DataAccess;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,6 +21,7 @@ namespace Shop
     public MainWindow()
     {
       InitializeComponent();
+      DGridShop.ItemsSource = ShopEntities.GetContext().shops.ToList();
     }
   }
 }
