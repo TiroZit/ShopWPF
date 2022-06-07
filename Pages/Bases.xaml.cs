44b25d3c4bf118b598e77ec276371094a0e0055f
TiroZit <nikus.master@gmail.com>
2022-06-07 17:33:22 +0500

    bases

--- Pages/Bases.xaml.cs
+++ Pages/Bases.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Shop.DataAccess;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,7 @@ namespace Shop.Pages
     public Bases()
     {
       InitializeComponent();
+      DGridBases.ItemsSource = ShopEntities.GetContext().base_product.ToList();
     }
   }
 }
